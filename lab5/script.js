4741fc9bb4ff856a2b112e262838c76b0aa16c5d
Lily Barrett <11barrettpl@gmail.com>
2023-08-12 14:30:34 +1200

    Added a dark mode button

---
+++ lab5/script.js
@@ -0,0 +1,10 @@
+function myFunction () {
+    var element = document.body;
+    element.classList.toggle("darkMode");
+   // element.classList.toggle("darkBtn");//
+
+   var Button = document.getElementById("btn");
+   Button.classList.toggle("darkBtn")
+}
+
+
