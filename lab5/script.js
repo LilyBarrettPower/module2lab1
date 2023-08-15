38737b067cc69ffc810fdaeff953319964a75476
Lily Barrett <11barrettpl@gmail.com>
2023-08-16 09:51:50 +1200

    Added comments to my code

--- lab5/script.js
+++ lab5/script.js
@@ -1,10 +1,13 @@
 function myFunction () {
     var element = document.body;
     element.classList.toggle("darkMode");
-   // element.classList.toggle("darkBtn");//
+//    Creating the function to toggle between dark mode and light mode
 
    var Button = document.getElementById("btn");
    Button.classList.toggle("darkBtn")
+//    Creating the function to toggle the dark mode button to be included in the dark mode. 
+//    Originally the button stayed light when the rest of the webpage altered.
 }
+
 
 
